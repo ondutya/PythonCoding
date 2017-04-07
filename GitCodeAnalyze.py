fe1dfd7f9450a71d66b70bf9b7a45b411e84d8d5
onduty <onduty@qq.com>
2017-04-08 00:20:59 +0800

    add py

---
+++ GitCodeAnalyze.py
@@ -0,0 +1,15 @@
+#! /usr/bin/env python
+
+# -*- coding: utf-8 -*-
+
+import os
+import xlrd
+import xlwt
+
+
+def git_code_analyze():
+    pass
+
+
+if __name__ == "__main__":
+    git_code_analyze()
